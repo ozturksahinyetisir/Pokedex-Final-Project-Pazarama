bbcbbf11c2fa3b68fca08cdeff0d481f8b0ebb16
s <ozturksahinyetisir@gmail.com>
2023-11-23 19:47:26 +0300

    Update RepositoryTest.kt

--- PokedexCompose/app/src/test/java/com/ozturksahinyetisir/pokedex/data/repository/PokemonRepositoryTest.kt
+++ PokedexCompose/app/src/test/java/com/ozturksahinyetisir/pokedex/data/repository/PokemonRepositoryTest.kt
@@ -13,11 +13,14 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.junit.MockitoRule
 
 @ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class PokemonRepositoryTest {
 
     @get:Rule
